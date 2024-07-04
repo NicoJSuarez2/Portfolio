e2542c0772d760dfdee982a33dce6a10e7f177c5
Nicolas Suarez <nsuarezz@poligran.edu.co>
2024-07-04 23:04:50 +0000

    HEADCHANGES

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/IM4.jpg";
+import image from "../images/IM2.jpg";
 
 const imageAltText = "lights of the city at night in a modern art museum.";
 
